890e4223273692704fa22c893b862ee9d8a8c643
anirranjan <anirranjan@gmail.com>
2024-07-19 16:44:46 -0500

    nirranjan: display all 20 questions of school level

--- client/src/components/Gameboard.js
+++ client/src/components/Gameboard.js
@@ -1,42 +1,147 @@
-import { useState } from 'react'
-import { useLocation } from 'react-router-dom';
-import { Box, Image, Heading, Text, Button } from '@chakra-ui/react'
-import startImage from '../SchoolTheme/languageschool-start.png';
-import image1 from '../SchoolTheme/languageschool-1.png';
-import image2 from '../SchoolTheme/languageschool-2.png';
-import image3 from '../SchoolTheme/languageschool-3.png';
-import image4 from '../SchoolTheme/languageschool-4.png';
-
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Image,
+  Heading,
+  Text,
+  Button,
+  Fade,
+  Center,
+  Input,
+} from "@chakra-ui/react";
+import startImage from "../SchoolTheme/languageschool-start.png";
+import image1 from "../SchoolTheme/languageschool-1.png";
+import image2 from "../SchoolTheme/languageschool-2.png";
+import image3 from "../SchoolTheme/languageschool-3.png";
+import image4 from "../SchoolTheme/languageschool-4.png";
+import image5 from "../SchoolTheme/languageschool-5.png";
+import image6 from "../SchoolTheme/languageschool-6.png";
+import image7 from "../SchoolTheme/languageschool-7.png";
+import image8 from "../SchoolTheme/languageschool-8.png";
+import image9 from "../SchoolTheme/languageschool-9.png";
+import image10 from "../SchoolTheme/languageschool-10.png";
+import image11 from "../SchoolTheme/languageschool-11.png";
+import image12 from "../SchoolTheme/languageschool-12.png";
+import image13 from "../SchoolTheme/languageschool-13.png";
+import image14 from "../SchoolTheme/languageschool-14.png";
+import image15 from "../SchoolTheme/languageschool-15.png";
+import image16 from "../SchoolTheme/languageschool-16.png";
+import image17 from "../SchoolTheme/languageschool-17.png";
+import image18 from "../SchoolTheme/languageschool-18.png";
+import image19 from "../SchoolTheme/languageschool-19.png";
 
 const GameBoard = () => {
-    const location = useLocation();
-    const { questions } = location.state || {};
+  const location = useLocation();
+  const { questions } = location.state || {};
 
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0)
-    const images = [startImage, image1, image2, image3, image4];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [showQuestion, setShowQuestion] = useState(true);
+  const images = [
+    startImage,
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+  ];
 
-    const handleNextQuestion = () => {
-        if (currentQuestionIndex < questions.length - 1) {
-            setCurrentQuestionIndex(currentQuestionIndex + 1)
-        }
-    }
+  const handleNextQuestion = () => {
+    setShowQuestion(false);
+    setTimeout(() => {
+      if (currentQuestionIndex < questions.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+      }
+      setShowQuestion(true);
+    }, 500); // Adjust the timeout to match your desired transition duration
+  };
 
-    const currentQuestion = questions[currentQuestionIndex];
-    const currentImage = images[currentQuestionIndex];
-
-    return (
-        <Box p={8}>
-            <Heading size="2xl">Question {currentQuestionIndex+1} </Heading>
-            <Image boxSize="800px" src={currentImage} alt="Game Image" />
-            <Text>{currentQuestion.scenario}</Text>
-            <ul>
-                {currentQuestion.answer_choices.map((choice) => (
-                    <li>{choice}</li>
-                ))}
-            </ul>
-            <Button colorScheme='blue' size='md' onClick={handleNextQuestion}>Next Question</Button>
+  const renderQuestion = () => {
+    if (currentQuestionIndex < 5) {
+      return (
+        <ul>
+          {currentQuestion.answer_choices.map((choice, index) => (
+            <li key={index}>{choice}</li>
+          ))}
+        </ul>
+      );
+    } else if (currentQuestionIndex < 10) {
+      return <Input placeholder="Your answer" />;
+    } else {
+      return (
+        <Box>
+          <Input placeholder="Your answer" mb={4} />
+          <ul>
+            {currentQuestion.answer_choices.map((choice, index) => (
+              <li key={index}>{choice}</li>
+            ))}
+          </ul>
         </Box>
-    )
-}
+      );
+    }
+  };
 
-export default GameBoard
+  const currentQuestion = questions[currentQuestionIndex];
+  const currentImage = images[currentQuestionIndex];
+
+  return (
+    <Center h="100vh">
+      <Box position="relative">
+        <Image
+          src={currentImage}
+          alt="Game Image"
+          objectFit="cover"
+          transition="opacity 0.5s ease-in-out"
+        />
+        <Fade in={showQuestion}>
+          <Box
+            position="absolute"
+            top="50%"
+            left="50%"
+            transform="translate(-50%, -50%)"
+            bg="rgba(0, 0, 0, 0.6)"
+            color="white"
+            p={20}
+            borderRadius="md"
+            zIndex="1"
+            textAlign="center"
+          >
+            <Heading size="2xl" mb={4}>
+              Question {currentQuestionIndex + 1}
+            </Heading>
+            <Text mb={4}>{currentQuestion.scenario}</Text>
+            {renderQuestion()}
+          </Box>
+        </Fade>
+        <Button
+          colorScheme="blue"
+          size="md"
+          onClick={handleNextQuestion}
+          position="absolute"
+          bottom="20px"
+          left="50%"
+          transform="translateX(-50%)"
+          zIndex="2"
+        >
+          Next Question
+        </Button>
+      </Box>
+    </Center>
+  );
+};
+
+export default GameBoard;
--- client/src/components/SchoolGame.js
+++ client/src/components/SchoolGame.js
@@ -2,26 +2,38 @@ import axios from "axios";
 import introImg from "../SchoolTheme/1 School Intro.jpeg";
 import { Heading, Text, Image, Button, Box } from '@chakra-ui/react'
 import { useNavigate } from "react-router-dom"
-import { useState } from "react"
 
 const SchoolGame = () => {
   const navigate = useNavigate();
-  const [questions, setQuestions] = useState([])
 
-  const getLevel1Questions = async () => {
+  const getSchoolQuestions = async (level) => {
     try {
-      const res = await axios.post("http://localhost:3001/schoollevel1", {
+      const res = await axios.post(`http://localhost:3001/schoollevel${level}`, {
         targetLanguage: "Spanish",
       });
       //console.log(typeof res.data)
       const responseData = JSON.parse(res.data);
       console.log(responseData)
-      setQuestions(responseData)
-      navigate('/gameboard', { state: { questions: responseData } })
+      return responseData
     } catch (error) {
-      console.error("Error:", error);
+      console.error("getSchoolQuestions | Error:", error);
     }
   };
+
+  const getAllQuestions = async () => {
+    try {
+      const level1Questions = await getSchoolQuestions(1);
+      const level2Questions = await getSchoolQuestions(2);
+      const level3Questions = await getSchoolQuestions(3);
+      const level4Questions = await getSchoolQuestions(4);
+
+      const allQuestions = [...level1Questions, ...level2Questions, ...level3Questions, ...level4Questions];
+      navigate("/gameboard", { state: { questions: allQuestions } });
+    } catch (error) {
+      console.error("getAllQuestions | Error:", error);
+    }
+  }
+
 
   return (
     <Box p={8}>
@@ -31,7 +43,7 @@ const SchoolGame = () => {
         Hi, I'm Bill. Today's my first day at my new school and I don't know
         Spanish very well. Can you help me find my new friends?
       </Text>
-      <Button colorScheme='blue' size='md' onClick={getLevel1Questions}>Start Game</Button>
+      <Button colorScheme='blue' size='md' onClick={getAllQuestions}>Start Game</Button>
     </Box>
   );
 };
